cc09ac671596f1002f953d9a0bbbf28287bb1e3c
Takenoko <takenoko.tech@gmail.com>
2020-01-18 17:12:06 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import "./extensions/Array.extensions";
-import "./extensions/Boolean.extensions";
-import "./extensions/Number.extensions";
+// import "./extensions/Boolean.extensions";
+// import "./extensions/Number.extensions";
 import "./extensions/Object.extensions";
-import "./extensions/String.extensions";
+// import "./extensions/String.extensions";
 
 import "./result/runCatching.result";
