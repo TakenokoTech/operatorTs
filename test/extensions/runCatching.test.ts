6b0d1297ec34d2b3b55b6e586794f6d21df40a95
Takenoko <takenoko.tech@gmail.com>
2020-01-18 14:13:03 +0900

    fix test

--- test/extensions/runCatching.test.ts
+++ test/extensions/runCatching.test.ts
@@ -10,66 +10,64 @@ const assertError = async (block: Promise<any>) => {
     }
 };
 
-describe("Extensions", async () => {
-    describe("runCatching()", async () => {
-        it("success: String", async () => {
-            const a: String = "test";
-            const expected = `arg=test is success`;
-            const actual = await a.runCatching(arg => `arg=${arg} is success`);
-            assert.equal(expected, actual);
-            // console.log(`${expected}`, `${actual}`);
-        });
+describe("Extensions runCatching()", () => {
+    it("success: String", async () => {
+        const a: String = "test";
+        const expected = `arg=test is success`;
+        const actual = await a.runCatching(arg => `arg=${arg} is success`);
+        assert.equal(expected, actual);
+        // console.log(`${expected}`, `${actual}`);
+    });
 
-        it("error: String", async () => {
-            const a: String = "test";
-            const expected = new Error(`arg=test is error`);
-            const actual = await assertError(
-                a.runCatching(arg => {
-                    throw new Error(`arg=${arg} is error`);
-                }),
-            );
-            assert.equal(`${expected}`, `${actual}`);
-            // console.log(`${expected}`, `${actual}`);
-        });
+    it("error: String", async () => {
+        const a: String = "test";
+        const expected = new Error(`arg=test is error`);
+        const actual = await assertError(
+            a.runCatching(arg => {
+                throw new Error(`arg=${arg} is error`);
+            }),
+        );
+        assert.equal(`${expected}`, `${actual}`);
+        // console.log(`${expected}`, `${actual}`);
+    });
 
-        it("success: Number", async () => {
-            const a: Number = 111;
-            const expected = `arg=111 is success`;
-            const actual = await a.runCatching(arg => `arg=${arg} is success`);
-            assert.equal(expected, actual);
-            // console.log(`${expected}`, `${actual}`);
-        });
+    it("success: Number", async () => {
+        const a: Number = 111;
+        const expected = `arg=111 is success`;
+        const actual = await a.runCatching(arg => `arg=${arg} is success`);
+        assert.equal(expected, actual);
+        // console.log(`${expected}`, `${actual}`);
+    });
 
-        it("error: Number", async () => {
-            const a: Number = 111;
-            const expected = new Error(`arg=111 is error`);
-            const actual = await assertError(
-                a.runCatching(arg => {
-                    throw new Error(`arg=${arg} is error`);
-                }),
-            );
-            assert.equal(`${expected}`, `${actual}`);
-            // console.log(`${expected}`, `${actual}`);
-        });
+    it("error: Number", async () => {
+        const a: Number = 111;
+        const expected = new Error(`arg=111 is error`);
+        const actual = await assertError(
+            a.runCatching(arg => {
+                throw new Error(`arg=${arg} is error`);
+            }),
+        );
+        assert.equal(`${expected}`, `${actual}`);
+        // console.log(`${expected}`, `${actual}`);
+    });
 
-        it("success: Promise", async () => {
-            const a: Promise<String> = Promise.resolve("test");
-            const expected = `arg=test is success`;
-            const actual = await a.runCatching(arg => `arg=${arg} is success`);
-            assert.equal(expected, actual);
-            // console.log(`${expected}`, `${actual}`);
-        });
+    it("success: Promise", async () => {
+        const a: Promise<String> = Promise.resolve("test");
+        const expected = `arg=test is success`;
+        const actual = await a.runCatching(arg => `arg=${arg} is success`);
+        assert.equal(expected, actual);
+        // console.log(`${expected}`, `${actual}`);
+    });
 
-        it("error: Promise", async () => {
-            const a: Promise<String> = Promise.resolve("test");
-            const expected = new Error(`arg=test is error`);
-            const actual = await assertError(
-                a.runCatching(arg => {
-                    throw new Error(`arg=${arg} is error`);
-                }),
-            );
-            assert.equal(`${expected}`, `${actual}`);
-            // console.log(`${expected}`, `${actual}`);
-        });
+    it("error: Promise", async () => {
+        const a: Promise<String> = Promise.resolve("test");
+        const expected = new Error(`arg=test is error`);
+        const actual = await assertError(
+            a.runCatching(arg => {
+                throw new Error(`arg=${arg} is error`);
+            }),
+        );
+        assert.equal(`${expected}`, `${actual}`);
+        // console.log(`${expected}`, `${actual}`);
     });
 });
--- test/main.test.ts
+++ test/main.test.ts
@@ -1,9 +1,9 @@
 import assert from "power-assert";
 import TestTarget from "../src/main";
 
-// describe("Main class", () => {
-//     it("constructor", () => {
-//         let testTarget = new TestTarget();
-//         assert.equal(testTarget.name, "Main");
-//     });
-// });
+describe("Main class", () => {
+    it("constructor", () => {
+        let testTarget = new TestTarget();
+        assert.equal(testTarget.name, "Main");
+    });
+});
