e30afb4b0f6f99674fb03872e6b817aec08e08df
Takenoko <takenoko.tech@gmail.com>
2020-01-20 10:53:13 +0900

    Delete main.test.ts

--- test/main.test.ts
+++
@@ -1,9 +0,0 @@
-import assert from "power-assert";
-import TestTarget from "../src/main";
-
-describe("Main class", () => {
-    it("constructor", () => {
-        let testTarget = new TestTarget();
-        assert.equal(testTarget.name, "Main");
-    });
-});
